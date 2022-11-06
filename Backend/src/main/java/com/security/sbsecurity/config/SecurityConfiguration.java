929f4aa521e454f4b7e76123b8a63c8da8acc9ae
diwangaamasith <diwangaamasith@gmail.com>
2022-11-06 12:53:32 +0530

    Inmemory Authentication Done with Form Login

---
+++ Backend/src/main/java/com/security/sbsecurity/config/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package com.security.sbsecurity.config;
+
+import org.hibernate.annotations.Type;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//this  is old way
+    //TODO refactor this to newer chaining way
+
+    @Bean//Spring @Bean Annotation is applied on a method to specify that it returns a bean to be managed by Spring context. Spring Bean annotation is usually declared in Configuration classes methods.
+    @Scope("singleton")
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.inMemoryAuthentication()
+               .withUser("pawani")
+               .password(passwordEncoder().encode("pawani123"))
+               .authorities("USER","ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+//  http.authorizeRequests().anyRequest().permitAll(); // giving public access to all the urls
+    http.authorizeRequests().anyRequest().authenticated();
+    http.formLogin();
+
+    }
+
+//    @Override
+//    protected AuthenticationManager authenticationManager() throws Exception {
+//        return super.authenticationManager();
+//    }
+}
